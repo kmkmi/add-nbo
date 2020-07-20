eb068fd149b2084532a11c14fb15ac2d187e6ba2
kmkmi <jk787787@naver.com>
2020-07-20 00:05:14 -0400

    Edit printf func.

--- main.c
+++ main.c
@@ -9,8 +9,8 @@ int main(int argc, char* argv[]){
 	FILE *fd1, *fd2;
 	
 
-	uint32_t* fst ;
-	uint32_t* snd ;
+	uint32_t fst ;
+	uint32_t snd ;
 
 	char buf1[4];
 	char buf2[4];
@@ -26,13 +26,13 @@ int main(int argc, char* argv[]){
 	read_check(buf1, fd1);
 	read_check(buf2, fd2);
 
-	fst =  (uint32_t*)(buf1);
-	snd = (uint32_t*)(buf2);
+	fst = ntohl(*((uint32_t*)buf1));
+	snd = ntohl(*((uint32_t*)buf2));
 
 	printf("%d(%#x)  +   %d(%#x)   =   %d(%#x)\n", 
-		ntohl(*fst) , ntohl(*fst), 
-			ntohl(*snd) , ntohl(*snd), 
-				ntohl(*fst)+ntohl(*snd) , ntohl(*fst)+ ntohl(*snd));
+		fst , fst, 
+			snd , snd, 
+				fst + snd , fst + snd);
 
 	fclose(fd2);
 	fclose(fd1);
